9fde6996f23a297ba836e465e07f102c87ae848f
YaelAram <yaelaram14@outlook.com>
2020-10-20 17:43:32 -0500

    Karnaugh map completed, now I'll test Karnaugh map class and converter class with JUnit

--- src/com/yaelaram/KarnaughMap.java
+++ src/com/yaelaram/KarnaughMap.java
@@ -10,10 +10,13 @@ public class KarnaughMap implements MouseListener, FocusListener, ActionListener
     private JButton createTable, clean, calculate;
     private JLabel alertDialog;
     private final ColorStyle colorStyle = new ColorStyle();
+    private JTextField[] rowTruthTable;
+    private JTextField[] outputTruthTable;
+    private JTextField[][] karnaughMapLabel;
 
     public KarnaughMap(){
         f = new JFrame();
-        f.setBounds(180, 10, 720, 700);
+        f.setBounds(180, 10, 920, 720);
         f.setLayout(null);
         f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         f.setTitle("Karnaugh Map");
@@ -35,13 +38,14 @@ public class KarnaughMap implements MouseListener, FocusListener, ActionListener
         f.add(numVariableLabel);
 
         numVariable = new JTextField();
-        numVariable.setBounds(20, 65, 100, 30);
+        numVariable.setBounds(20, 65, 60, 30);
         numVariable.addFocusListener(this);
+        numVariable.addActionListener(this);
         textFieldStyle(numVariable, ColorStyle.INACTIVE_TEXT_FIELD_FLAG);
         f.add(numVariable);
 
         createTable = new JButton("Create Table");
-        createTable.setBounds(160, 60, 150, 40);
+        createTable.setBounds(190, 60, 150, 40);
         createTable.setFocusable(false);
         createTable.addMouseListener(this);
         createTable.addActionListener(this);
@@ -49,15 +53,16 @@ public class KarnaughMap implements MouseListener, FocusListener, ActionListener
         f.add(createTable);
 
         calculate = new JButton("Calculate");
-        calculate.setBounds(340, 60, 150, 40);
+        calculate.setBounds(440, 60, 150, 40);
         calculate.setFocusable(false);
+        calculate.setEnabled(false);
         calculate.addMouseListener(this);
         calculate.addActionListener(this);
         buttonStyle(calculate, ColorStyle.INACTIVE_BUTTON_FLAG);
         f.add(calculate);
 
         clean = new JButton("Clean");
-        clean.setBounds(520, 60, 150, 40);
+        clean.setBounds(690, 60, 150, 40);
         clean.setFocusable(false);
         clean.addMouseListener(this);
         clean.addActionListener(this);
@@ -65,7 +70,7 @@ public class KarnaughMap implements MouseListener, FocusListener, ActionListener
         f.add(clean);
 
         alertDialog = new JLabel();
-        alertDialog.setBounds(20, 120, 680, 30);
+        alertDialog.setBounds(20, 110, 900, 30);
         alertDialogStyle(alertDialog, "");
         f.add(alertDialog);
     }
@@ -115,19 +120,302 @@ public class KarnaughMap implements MouseListener, FocusListener, ActionListener
         jLabel.setText(message);
     }
 
+    //solveMap Method
+    private String solveMap(int numRow, int[] outputValues){
+        StringBuilder stringBuilder = new StringBuilder();
+        String divider = " + ";
+        for(int i = 0 ; i<numRow ; i++){
+            if(outputValues[i] == 1){
+                if(i != 0){
+                    stringBuilder.append(divider);
+                }
+                String values = rowTruthTable[i].getText();
+                for(int j = 0 ; j<values.length() ; j++){
+                    if(j == 0){
+                        if(values.charAt(j) == '1'){
+                            stringBuilder.append("A");
+                        }
+                        else{
+                            stringBuilder.append("\u0100");
+                        }
+                    }
+                    else if(j == 1){
+                        if(values.charAt(j) == '1'){
+                            stringBuilder.append("E");
+                        }
+                        else{
+                            stringBuilder.append("\u0112");
+                        }
+                    }
+                    else if(j == 2){
+                        if(values.charAt(j) == '1'){
+                            stringBuilder.append("O");
+                        }
+                        else{
+                            stringBuilder.append("\u014C");
+                        }
+                    }
+                    else{
+                        if(values.charAt(j) == '1'){
+                            stringBuilder.append("U");
+                        }
+                        else{
+                            stringBuilder.append("\u016A");
+                        }
+                    }
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private int[][] createKarnaughMap(int numColumn, int numRow, int[] outputValues){
+        int[][] resultArray;
+        if(numColumn == 2){
+            resultArray = new int[numColumn][numColumn];
+            for(int i = 0 ; i<numRow ; i++){
+                if(outputValues[i] == 1){
+                    String values = rowTruthTable[i].getText();
+                    String aColumn = String.valueOf(values.charAt(0));
+                    String eRow = String.valueOf(values.charAt(1));
+                    resultArray[Integer.parseInt(eRow)][Integer.parseInt(aColumn)] = 1;
+                }
+            }
+        }
+        else if(numColumn == 3){
+            resultArray = new int[numColumn - 1][numColumn + 1];
+            Converter converter = new Converter();
+            for(int i = 0 ; i<numRow ; i++){
+                if(outputValues[i] == 1){
+                    String values = rowTruthTable[i].getText();
+                    String aColumn = String.valueOf(values.charAt(0));
+                    String eColumn = String.valueOf(values.charAt(1));
+                    String oRow = String.valueOf(values.charAt(2));
+                    long column = converter.toDecimal((aColumn + eColumn), 2);
+                    if(column == 3){
+                        column = 2;
+                    }
+                    else if(column == 2){
+                        column = 3;
+                    }
+                    resultArray[Integer.parseInt(oRow)][(int) column] = 1;
+                }
+            }
+        }
+        else{
+            resultArray = new int[numColumn + 1][numColumn + 1];
+            Converter converter = new Converter();
+            for(int i = 0 ; i<numRow ; i++){
+                if(outputValues[i] == 1){
+                    String values = rowTruthTable[i].getText();
+                    String aColumn = String.valueOf(values.charAt(0));
+                    String eColumn = String.valueOf(values.charAt(1));
+                    String oRow = String.valueOf(values.charAt(2));
+                    String uRow = String.valueOf(values.charAt(3));
+                    long column = converter.toDecimal((aColumn + eColumn), 2);
+                    long row = converter.toDecimal((oRow + uRow), 2);
+                    if(column == 3){
+                        column = 2;
+                    }
+                    else if(column == 2){
+                        column = 3;
+                    }
+                    if(row == 3){
+                        row = 2;
+                    }
+                    else if(row == 2){
+                        row = 3;
+                    }
+                    resultArray[(int)row][(int) column] = 1;
+                }
+            }
+        }
+        return resultArray;
+    }
+
     //OnClick Event
     @Override
     public void actionPerformed(ActionEvent evt) {
         try{
             if(evt.getSource() == clean){
-                numVariable.setText("");
-                alertDialogStyle(alertDialog, "");
+                f.dispose();
+                new KarnaughMap();
             }
-            if(evt.getSource() == createTable){
-                alertDialogStyle(alertDialog,"Creating table with " + numVariable.getText() + " variables...");
+            if(evt.getSource() == createTable || evt.getSource() == numVariable){
+                if(!numVariable.getText().isEmpty() || !numVariable.getText().isBlank()){
+                    try{
+                        int numColumn = Integer.parseInt(numVariable.getText());
+                        alertDialogStyle(alertDialog, "");
+                        if(numColumn <= 1 || numColumn > 4){
+                            alertDialogStyle(alertDialog, "Please only numbers from 2 to 4");
+                            calculate.setEnabled(false);
+                        }
+                        else{
+                            int numRow = (int) Math.pow(2, numColumn);
+                            rowTruthTable = new JTextField[numRow];
+                            outputTruthTable = new JTextField[numRow];
+                            Converter converter = new Converter();
+                            JLabel[] horizontalTitle, verticalTitle;
+                            for(int i = 0 ; i<numRow ; i++){
+                                StringBuilder stringBuilder = new StringBuilder(converter.fromDecimal(i, 2));
+                                while(stringBuilder.length() < numColumn){
+                                    stringBuilder.insert(0, "0");
+                                }
+                                rowTruthTable[i] = new JTextField(stringBuilder.toString());
+                                rowTruthTable[i].setBounds(20, (140 + (32 * i)), 70, 30);
+                                textFieldStyle(rowTruthTable[i], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                rowTruthTable[i].setEditable(false);
+                                rowTruthTable[i].setFocusable(false);
+                                rowTruthTable[i].setHorizontalAlignment(SwingConstants.CENTER);
+                                f.add(rowTruthTable[i]);
+                                outputTruthTable[i] = new JTextField();
+                                outputTruthTable[i].setBounds(110, (140 + (32 * i)), 30, 30);
+                                textFieldStyle(outputTruthTable[i], ColorStyle.INACTIVE_TEXT_FIELD_FLAG);
+                                outputTruthTable[i].setHorizontalAlignment(SwingConstants.CENTER);
+                                outputTruthTable[i].addFocusListener(this);
+                                f.add(outputTruthTable[i]);
+                            }
+                            if(numColumn == 2){
+                                karnaughMapLabel = new JTextField[2][2];
+                                horizontalTitle = new JLabel[2];
+                                verticalTitle = new JLabel[2];
+                                for(int i = 0 ; i<2 ; i++){
+                                    for(int j = 0 ; j<2 ; j++){
+                                        horizontalTitle[j] = new JLabel("A - " + j);
+                                        horizontalTitle[j].setBounds((320 + (70 * j)), 190, 30, 30);
+                                        labelStyle(horizontalTitle[j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(horizontalTitle[j]);
+                                        verticalTitle[j] = new JLabel("E - " + j);
+                                        verticalTitle[j].setBounds(260, (220 + (65 * j)), 30, 30);
+                                        labelStyle(verticalTitle[j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(verticalTitle[j]);
+                                        karnaughMapLabel[i][j] = new JTextField();
+                                        karnaughMapLabel[i][j].setBounds((320 + (70 * j)), (220 + (60 * i)), 30, 30);
+                                        karnaughMapLabel[i][j].setFocusable(false);
+                                        karnaughMapLabel[i][j].setEditable(false);
+                                        karnaughMapLabel[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+                                        textFieldStyle(karnaughMapLabel[i][j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(karnaughMapLabel[i][j]);
+                                    }
+                                }
+                            }
+                            else if(numColumn == 3){
+                                horizontalTitle = new JLabel[4];
+                                verticalTitle = new JLabel[2];
+                                karnaughMapLabel = new JTextField[2][4];
+                                String[] title = {"AE-00", "AE-01", "AE-11", "AE-10"};
+                                for(int i = 0 ; i<2 ; i++){
+                                    for(int j = 0 ; j<4 ; j++){
+                                        horizontalTitle[j] = new JLabel(title[j]);
+                                        horizontalTitle[j].setBounds((320 + (70 * j)), 190, 50, 30);
+                                        horizontalTitle[j].setHorizontalAlignment(SwingConstants.CENTER);
+                                        labelStyle(horizontalTitle[j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(horizontalTitle[j]);
+                                        verticalTitle[i] = new JLabel("O - " + i);
+                                        verticalTitle[i].setBounds(260, (225 + (60 * i)), 35, 30);
+                                        labelStyle(verticalTitle[i], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(verticalTitle[i]);
+                                        karnaughMapLabel[i][j] = new JTextField();
+                                        karnaughMapLabel[i][j].setBounds((320 + (70 * j)), (220 + (60 * i)), 50, 30);
+                                        karnaughMapLabel[i][j].setFocusable(false);
+                                        karnaughMapLabel[i][j].setEditable(false);
+                                        karnaughMapLabel[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+                                        textFieldStyle(karnaughMapLabel[i][j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(karnaughMapLabel[i][j]);
+                                    }
+                                }
+                            }
+                            else{
+                                horizontalTitle = new JLabel[4];
+                                verticalTitle = new JLabel[4];
+                                String[] titleHorizontal = {"AE-00", "AE-01", "AE-11", "AE-10"};
+                                String[] titleVertical = {"OU-00", "OU-01", "OU-11", "OU-10"};
+                                karnaughMapLabel = new JTextField[4][4];
+                                for(int i = 0 ; i<4 ; i++){
+                                    for(int j = 0 ; j<4 ; j++){
+                                        horizontalTitle[j] = new JLabel(titleHorizontal[j]);
+                                        horizontalTitle[j].setBounds((320 + (70 * j)), 190, 50, 30);
+                                        horizontalTitle[j].setHorizontalAlignment(SwingConstants.CENTER);
+                                        labelStyle(horizontalTitle[j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(horizontalTitle[j]);
+                                        verticalTitle[j] = new JLabel(titleVertical[j]);
+                                        verticalTitle[j].setBounds(250, (225 + (60 * j)), 50, 30);
+                                        labelStyle(verticalTitle[j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(verticalTitle[j]);
+                                        karnaughMapLabel[i][j] = new JTextField();
+                                        karnaughMapLabel[i][j].setBounds((320 + (70 * j)), (220 + (60 * i)), 50, 30);
+                                        karnaughMapLabel[i][j].setFocusable(false);
+                                        karnaughMapLabel[i][j].setEditable(false);
+                                        karnaughMapLabel[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+                                        textFieldStyle(karnaughMapLabel[i][j], ColorStyle.ACTIVE_TEXT_FIELD_FLAG);
+                                        f.add(karnaughMapLabel[i][j]);
+                                    }
+                                }
+                            }
+                            f.update(f.getGraphics());
+                            createTable.setEnabled(false);
+                            calculate.setEnabled(true);
+                        }
+                    }
+                    catch (NumberFormatException error){
+                        System.out.println("Karnaugh class calculate button: " + error.getMessage());
+                        alertDialogStyle(alertDialog, "Please only int numbers (2 - 4).");
+                    }
+                }
+                else{
+                    alertDialogStyle(alertDialog, "Please enter a number.");
+                }
             }
             if(evt.getSource() == calculate){
-                alertDialogStyle(alertDialog, "Solving...");
+                int flag = 0;
+                int numColumn = Integer.parseInt(numVariable.getText());
+                int numRow = (int) Math.pow(2, numColumn);
+                int[] outputValues = new int[numRow];
+                for(int i = 0 ; i<numRow ; i++){
+                    if(outputTruthTable[i].getText().isBlank() || outputTruthTable[i].getText().isEmpty()){
+                        flag++;
+                    }
+                    else{
+                        try{
+                            outputValues[i] = Integer.parseInt(outputTruthTable[i].getText());
+                            if(outputValues[i] != 0 && outputValues[i] != 1){
+                                flag++;
+                            }
+                        }
+                        catch(NumberFormatException error){
+                            System.out.println("Karnaugh map class calculate button: " + error.getMessage());
+                            flag++;
+                        }
+                    }
+                }
+                if(flag == 0){
+                    alertDialogStyle(alertDialog, solveMap(numRow, outputValues));
+                    int[][] karnaughMap = createKarnaughMap(numColumn, numRow, outputValues);
+                    if(numColumn == 2){
+                        for(int i = 0 ; i<2 ; i++){
+                            for(int j = 0 ; j<2 ; j++){
+                                karnaughMapLabel[i][j].setText(String.valueOf(karnaughMap[i][j]));
+                            }
+                        }
+                    }
+                    else if(numColumn == 3){
+                        for(int i = 0 ; i<2 ; i++){
+                            for(int j = 0 ; j<4 ; j++){
+                                karnaughMapLabel[i][j].setText(String.valueOf(karnaughMap[i][j]));
+                            }
+                        }
+                    }
+                    else{
+                        for(int i = 0 ; i<4 ; i++){
+                            for(int j = 0 ; j<4 ; j++){
+                                karnaughMapLabel[i][j].setText(String.valueOf(karnaughMap[i][j]));
+                            }
+                        }
+                    }
+                }
+                else{
+                    alertDialogStyle(alertDialog, "Please enter all fields and only numbers from 0 to 1.");
+                }
             }
         }
         catch (Exception e){
